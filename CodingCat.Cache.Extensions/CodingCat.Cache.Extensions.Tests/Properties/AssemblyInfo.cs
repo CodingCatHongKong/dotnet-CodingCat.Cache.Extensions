5817de2d23e61d1cdb32a5e261d9fb1a1430f068
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-19 21:37:00 +0800

    added .Net 4.6.1 test project

---
+++ CodingCat.Cache.Extensions/CodingCat.Cache.Extensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodingCat.Cache.Extensions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodingCat.Cache.Extensions.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d149017f-c441-4f58-a557-ab2a89cf2565")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
